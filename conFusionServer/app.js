fc2a9df180ae2a194ba96c01269fbd8cd1630101
Nguyen Minh Tri <zymethyang@gmail.com>
2017-10-21 16:57:52 +0700

    Express Session Part 1

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -29,46 +29,56 @@ connect.then((db) => {
 
 var app = express();
 
-app.use(cookieParser('12345-67890-09876-54321'));
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
-function auth (req, res, next) {
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
-  if (!req.signedCookies.user) {
+function auth(req, res, next) {
+  console.log(req.session);
+
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
-        err.status = 401;
-        next(err);
-        return;
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
     }
     var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
-        res.cookie('user','admin',{signed: true});
-        next(); // authorized
+      req.session.user = 'admin';
+      next(); // authorized
     } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
-        err.status = 401;
-        next(err);
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
     }
   }
   else {
-      if (req.signedCookies.user === 'admin') {
-          next();
-      }
-      else {
-          var err = new Error('You are not authenticated!');
-          err.status = 401;
-          next(err);
-      }
+    if (req.session.user === 'admin') {
+      console.log('req.session: ', req.session);
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      next(err);
+    }
   }
 }
 
 app.use(auth);
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
