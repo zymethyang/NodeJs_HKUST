9ee8416cabdcc65c6051e5fa4939908eb5d170c5
Nguyen Minh Tri <zymethyang@gmail.com>
2017-10-22 22:32:29 +0700

    Mongoose Population

--- conFusionServer/models/dishes.js
+++ conFusionServer/models/dishes.js
@@ -6,23 +6,23 @@ require('mongoose-currency').loadType(mongoose);
 var Currency = mongoose.Types.Currency;
 
 var commentSchema = new Schema({
-    rating: {
+    rating:  {
         type: Number,
         min: 1,
         max: 5,
         required: true
     },
-    comment: {
+    comment:  {
         type: String,
         required: true
     },
     author: {
-        type: String,
-        required: true
-    },
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 }, {
-        timestamps: true
-    });
+    timestamps: true
+});
 
 var dishSchema = new Schema({
     name: {
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
+
 router.use(bodyParser.json());
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -19,10 +20,22 @@ router.post('/signup', (req, res, next) => {
       res.json({err: err});
     }
     else {
-      passport.authenticate('local')(req, res, () => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({success: true, status: 'Registration Successful!'});
+      if (req.body.firstname)
+        user.firstname = req.body.firstname;
+      if (req.body.lastname)
+        user.lastname = req.body.lastname;
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+          return ;
+        }
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'Registration Successful!'});
+        });
       });
     }
   });
